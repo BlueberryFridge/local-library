629d9b1253151ad527fb9d0e51e48e639fca1bf4
Janine Lapiz <jglapiz@up.edu.ph>
2020-10-10 22:58:18 +0800

    Added create book template; edited bookController for post and get of create book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,6 +6,7 @@ const BookInstance = require('../models/bookinstance');
 const Genre = require('../models/genre');
 
 const async = require('async');
+const { body, validationResult } = require('express-validator');
 
 // Display home page
 exports.index = (req, res) => {
@@ -58,14 +59,77 @@ exports.book_detail = (req, res, next) => {
 }
 
 // Display Book create form on GET
-exports.book_create_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Book create GET');
+// a little more complication than Author and Genre because we need to get
+// and display available Author and Genre records in our Book form
+exports.book_create_get = (req, res, next) => {
+    // get all authors and genres, which we can use for adding to our book
+    async.parallel({
+        authors: callback => Author.find(callback),
+        genres: callback => Genre.find(callback)
+        }, (err, results) => {
+            if(err) return next(err);
+            res.render('book_form', { title:'Create Book',
+                                      authors: results.authors,
+                                      genres: results.genres });
+        });
 }
 
 // Display Book create on POST
-exports.book_create_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Book create POST');
-}
+exports.book_create_post = [
+    // convert genre to an array
+    (req, res, next) => {
+        if(!(req.body.genre instanceof Array)) {
+            if(typeof req.body.genre === 'undefined') req.body.genre = [];
+            else req.body.genre = new Array(req.body.genre);
+        }
+        next();
+    },
+    // validate and sanitise fields
+    body('title', 'Title must not be empty.').isLength({ min: 1 }).trim().escape(),
+    body('author', 'Author must not be empty.').isLength({ min: 1 }).trim().escape(),
+    body('summary', 'Summary must not be empty.').isLength({ min: 1 }).trim().escape(),
+    body('isbn', 'ISBN must not be empty.').isLength({ min: 1 }).trim().escape(),
+    body('genre.*').escape(),           // '*' individually validates each of the genre in the array entries
+    // process request
+    (req, res, next) => {
+        const errors = validationResult(req);
+        const book = new Book(
+            { title: req.body.title,
+              author: req.body.author,
+              summary: req.body.summary,
+              isbn: req.body.isbn,
+              genre: req.body.genre
+            });
+        if(!errors.isEmpty()) {
+            // there are errors; get all authors and genres for form
+            async.parallel({
+                authors: callback => Author.find(callback),
+                genres: callback => Genre.find(callback)
+                }, (err, results) => {
+                    if(err) return next(err);
+                    // mark selected genres as checked; pass in existing genres and authors to the form
+                    for(let i = 0; i < results.genres.length; i++) {
+                        if(book.genre.indexOf(results.genres[i]._id) > -1) {
+                            results.genres[i].checked = 'true';
+                        }
+                    }
+                    res.render('book_form', { title: 'Create Book',
+                                              authors: results.authors,
+                                              genres: results.genres,
+                                              book: book,
+                                              errors: errors.array() });           
+                });
+                return;
+        }
+        else {
+            // data from form is valid; save book
+            book.save( err => {
+                if(err) return next(err);
+                res.redirect(book.url);
+            });
+        }
+    }
+];
 
 // Display Book delete form on GET
 exports.book_delete_get = (req, res) => {
